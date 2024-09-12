1d10541f6842ba3288f164a99f65dd2690c49355
jbmdevelopment <jbarreramartinez100@student.hbuhsd>
2024-09-12 11:00:02 -0700

    commit3

--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,12 @@
-
+public class Problem3 {
+    public static void main(String[] args) {
+        double digitNum = 505.9246;
+        int castedNum = (int)(digitNum * 1000);
+        int first = (castedNum / 100) % 10;
+        int mid = (castedNum / 10) % 10;
+        int last = castedNum % 10;
+        System.out.print(first);
+        System.out.print(mid);
+        System.out.print(last);
+    }
+}
